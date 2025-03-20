17c9a8f466c5fecb62918e1a77eca5b00466d7ba
Divol3tech <divol3.tech@gmail.com>
2025-03-20 20:35:21 +0530

    3 Is commplet

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,13 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text("Hello world")
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text("I Am Rich",style: TextStyle(color: Colors.white),),centerTitle: true,
+          backgroundColor: Colors.blueGrey[900],
+        ),
+body: Center(child: Image(image: NetworkImage('https://avatars.mds.yandex.net/get-mpic/4355034/img_id8397060369969564559.jpeg/orig'))),
       )
     )
   );
