1225ac00da2723bd01c8fe841ef9a64232995241
Divol3tech <divol3.tech@gmail.com>
2025-03-21 15:25:19 +0530

    commpite

--- lib/main.dart
+++ lib/main.dart
@@ -6,10 +6,10 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.blueGrey,
         appBar: AppBar(
-          title: Text("I Am Rich",style: TextStyle(color: Colors.white),),centerTitle: true,
-          backgroundColor: Colors.blueGrey[900],
+          title: Text("I Am Rich",style: TextStyle(color: Colors.amberAccent,fontWeight: FontWeight.bold),),centerTitle: true,
+          backgroundColor: Colors.blueGrey[600],
         ),
-body: Center(child: Image(image: NetworkImage('https://avatars.mds.yandex.net/get-mpic/4355034/img_id8397060369969564559.jpeg/orig'))),
+body: Center(child: Image(image: AssetImage('assets/i_am_rich.jpg'),fit: BoxFit.fill,)),
       )
     )
   );
